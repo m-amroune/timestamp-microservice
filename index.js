4b4bfda13079b3808775b7c2426758ede22525b0
dev-front33 <31637994-dev-front33@users.noreply.replit.com>
2025-08-27 08:20:30 +0000

    feat: handle empty api route to return current time in unix and utc format

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function (req, res) {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  });
+});
+
 app.get("/api/:date", function (req, res) {
   const dateParam = req.params.date;
 
@@ -45,6 +53,9 @@ app.get("/api/:date", function (req, res) {
     utc: date.toUTCString()
   });
 });
+
+
+
 
 
 // Listen on port set in environment variable or default to 3000
